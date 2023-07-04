25ce43e967e722244f181f242855e9d3949d2a21
C0b22022 <c0b2202295@edu.teu.ac.jp>
2023-07-04 17:21:58 +0900

    追加機能1実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -105,6 +105,8 @@ class Bird(pg.sprite.Sprite):
     def get_direction(self) -> tuple[int, int]:
         return self.dire
     
+    
+
 
 class Bomb(pg.sprite.Sprite):
     """
@@ -265,11 +267,17 @@ def main():
     clock = pg.time.Clock()
     while True:
         key_lst = pg.key.get_pressed()
+    
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
+            if key_lst[pg.K_LSHIFT]:
+                bird.speed = 20
+            else:
+                bird.speed = 10
+                
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
@@ -295,7 +303,8 @@ def main():
             pg.display.update()
             time.sleep(2)
             return
-
+        
+        
         bird.update(key_lst, screen)
         beams.update()
         beams.draw(screen)
